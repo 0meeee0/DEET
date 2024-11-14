83f9ffc463a360e6e4c8214a7004582e79928978
0meeee0 <mehdi74.id@gmail.com>
2024-11-14 11:40:44 +0100

    Feat: Message in friends section to handle if no friend

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 
 export default function Friends() {
+  const friends = ["Nouamane"];
+
   return (
     <div className="">
-      <div className="cntrchat pb-2 ">Chat</div>
-      <div className="bg-gray-700 rounded-full w-60 fr my-2 shadow-lg">
-        <div className="sum ">
-          <div className="profile-img-container">
-            <img
-              className="profile-img"
-              src="https://intranet.youcode.ma/storage/users/profile/thumbnail/804-1696616151.jpg"
-              alt="Profile"
-            />
-            <div className="isOnline"></div>
-          </div>
-          <div className="profile-info">
-            <h3 className="username">Nouamane</h3>
-            <span className="last-msg">msg</span>
+      <div className="cntrchat pb-2">Chat</div>
+      {
+        friends.length > 0? (
+      friends?.map((friend, index) => (
+        <div className="bg-gray-700 rounded-full w-60 fr my-2 shadow-lg">
+          <div className="sum">
+            <div key={index} className="friend-container flex items-center">
+              <div className="profile-img-container relative">
+                <img
+                  className="profile-img"
+                  src="https://intranet.youcode.ma/storage/users/profile/thumbnail/804-1696616151.jpg"
+                  alt="Profile"
+                />
+                <div className="isOnline absolute bottom-0 right-0 bg-green-500 w-3 h-3 rounded-full border border-white"></div>
+              </div>
+              <div className="profile-info ml-4">
+                <h3 className="username text-white">{friend}</h3>
+                <span className="last-msg text-gray-400">msg</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ))
+    ):(<h1 className="flex justify-center my-20">Add Friends To Chat With 🫡</h1>) }
     </div>
   );
 }
